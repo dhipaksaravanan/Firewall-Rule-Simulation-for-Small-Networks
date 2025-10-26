87e39538c119c483f298e1aeb36446ccfc89f50c
dhipaksaravanan <dhipaksaravanan324@gmail.com>
2025-10-26 18:07:45 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FirewallSimulator from './components/FirewallSimulator';
+
+function App() {
+  return (
+    <div className="App">
+      <FirewallSimulator />
+    </div>
+  );
+}
+
+export default App;
